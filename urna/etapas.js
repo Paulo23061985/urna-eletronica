7b9b118a56ea66829c1514116e324cf81fdbfee3
PAULO ROBERTO MENEZES RODRIGUES <112999061+Paulo23061985@users.noreply.github.com>
2022-10-12 15:15:22 -0300

    urna eletronica
    vote para presidente na urna eletronica

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    /*{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },*/
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Jair Messias Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Neto',
+                fotos:[
+                    {url:'bolsonaro1.png', legenda: 'Presidente'},
+                    {url:'braganeto.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Luiz Inácio Lula da Silva',
+                partido: 'PT',
+                vice: 'Geraldo Alkmer',
+                fotos:[
+                    {url:'lula.jpg', legenda: 'Presidente'},
+                    {url:'alkmer.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
